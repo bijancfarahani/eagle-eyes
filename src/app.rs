91f7df0fb35eedf77b4d5bbdb91799b623fc0f45
Bijan Farahani <bijancfarahani@gmail.com>
2024-02-25 18:28:07 -0800

    randomize and display letters

--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,6 @@
 mod game_deck;
+use egui::{Button, Vec2};
+
 use crate::app::game_deck::game_deck::Deck;
 
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
@@ -7,15 +9,13 @@ use crate::app::game_deck::game_deck::Deck;
 pub struct TemplateApp {
     #[serde(skip)] // This how you opt-out of serialization of a field
     deck: Deck,
-
-
 }
 //ui.add(egui::Button::image(egui::Image::new(egui::include_image!("../assets/letters/e.png"))));
 
 impl Default for TemplateApp {
     fn default() -> Self {
         Self {
-            deck: game_deck::game_deck::initial_deck(),
+            deck: game_deck::game_deck::get_scrambled_deck(),
         }
     }
 }
@@ -28,15 +28,70 @@ impl TemplateApp {
         // This is used to support images.
         egui_extras::install_image_loaders(&cc.egui_ctx);
 
-
         // Load previous app state (if any).
         // Note that you must enable the `persistence` feature for this to work.
         if let Some(storage) = cc.storage {
             return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
         }
 
-
         Default::default()
+    }
+
+    fn central_panel(&self, ctx: &egui::Context) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            // The central panel the region left after adding TopPanel's and SidePanel's
+            ui.heading("Welcome to EAGLE EYES!");
+
+            ui.separator();
+
+            ui.add(egui::github_link_file!(
+                "https://github.com/bijancfarahani/eagle_eyes/master/",
+                "Source code."
+            ));
+
+            ui.separator();
+
+            // Spell out EAGLE EYES.
+            ui.horizontal_centered(|ui| {
+                let pic_size = Vec2 { x: 250.0, y: 250.0 };
+                let e_letter = egui::include_image!("../assets/letters/e.png");
+                let a_letter = egui::include_image!("../assets/letters/a.png");
+                let g_letter = egui::include_image!("../assets/letters/g.png");
+                let l_letter = egui::include_image!("../assets/letters/l.png");
+                let y_letter = egui::include_image!("../assets/letters/y.png");
+                let s_letter = egui::include_image!("../assets/letters/s.png");
+                let blank_image = egui::include_image!("../assets/letters/blank.png");
+
+                for card in self.deck {
+                    println!("adding card");
+                    match card.letter {
+                        'e' => ui.add(egui::Button::image(
+                            egui::Image::new(e_letter.clone()).max_size(pic_size),
+                        )),
+                        'a' => ui.add(egui::Button::image(
+                            egui::Image::new(a_letter.clone()).max_size(pic_size),
+                        )),
+                        'g' => ui.add(egui::Button::image(
+                            egui::Image::new(g_letter.clone()).max_size(pic_size),
+                        )),
+                        'l' => ui.add(egui::Button::image(
+                            egui::Image::new(l_letter.clone()).max_size(pic_size),
+                        )),
+                        'y' => ui.add(egui::Button::image(
+                            egui::Image::new(y_letter.clone()).max_size(pic_size),
+                        )),
+                        's' => ui.add(egui::Button::image(
+                            egui::Image::new(s_letter.clone()).max_size(pic_size),
+                        )),
+                        _ => panic!(),
+                    };
+                }
+                ui.with_layout(egui::Layout::top_down_justified(egui::Align::LEFT), |ui| {
+                    powered_by_egui_and_eframe(ui);
+                    egui::warn_if_debug_build(ui);
+                });
+            });
+        });
     }
 }
 
@@ -69,7 +124,7 @@ impl eframe::App for TemplateApp {
                 egui::widgets::global_dark_light_mode_buttons(ui);
             });
         });
-
+        // self.central_panel(ctx);
         egui::CentralPanel::default().show(ctx, |ui| {
             // The central panel the region left after adding TopPanel's and SidePanel's
             ui.heading("Welcome to EAGLE EYES!");
@@ -84,20 +139,47 @@ impl eframe::App for TemplateApp {
             ui.separator();
 
             // Spell out EAGLE EYES.
-            ui.add(egui::Button::image(egui::Image::new(egui::include_image!("../assets/letters/e.png"))));
-            ui.add(egui::Button::image(egui::Image::new(egui::include_image!("../assets/letters/g.png"))));
-            ui.add(egui::Button::image(egui::Image::new(egui::include_image!("../assets/letters/l.png"))));
-            ui.add(egui::Button::image(egui::Image::new(egui::include_image!("../assets/letters/e.png"))));
-            ui.add(egui::Button::image(egui::Image::new(egui::include_image!("../assets/letters/e.png"))));
-            ui.add(egui::Button::image(egui::Image::new(egui::include_image!("../assets/letters/y.png"))));
-            ui.add(egui::Button::image(egui::Image::new(egui::include_image!("../assets/letters/e.png"))));
-            ui.add(egui::Button::image(egui::Image::new(egui::include_image!("../assets/letters/s.png"))));
+            ui.horizontal_centered(|ui| {
+                let pic_size = Vec2 { x: 250.0, y: 250.0 };
+                let e_letter = egui::include_image!("../assets/letters/e.png");
+                let a_letter = egui::include_image!("../assets/letters/a.png");
+                let g_letter = egui::include_image!("../assets/letters/g.png");
+                let l_letter = egui::include_image!("../assets/letters/l.png");
+                let y_letter = egui::include_image!("../assets/letters/y.png");
+                let s_letter = egui::include_image!("../assets/letters/s.png");
+                let blank_image = egui::include_image!("../assets/letters/blank.png");
 
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                powered_by_egui_and_eframe(ui);
-                egui::warn_if_debug_build(ui);
+                for card in self.deck {
+                    println!("adding card");
+                    match card.letter.to_ascii_lowercase() {
+                        'e' => ui.add(egui::Button::image(
+                            egui::Image::new(e_letter.clone()).max_size(pic_size),
+                        )),
+                        'a' => ui.add(egui::Button::image(
+                            egui::Image::new(a_letter.clone()).max_size(pic_size),
+                        )),
+                        'g' => ui.add(egui::Button::image(
+                            egui::Image::new(g_letter.clone()).max_size(pic_size),
+                        )),
+                        'l' => ui.add(egui::Button::image(
+                            egui::Image::new(l_letter.clone()).max_size(pic_size),
+                        )),
+                        'y' => ui.add(egui::Button::image(
+                            egui::Image::new(y_letter.clone()).max_size(pic_size),
+                        )),
+                        's' => ui.add(egui::Button::image(
+                            egui::Image::new(s_letter.clone()).max_size(pic_size),
+                        )),
+                        _ => ui.add(egui::Button::image(
+                            egui::Image::new(blank_image.clone()).max_size(pic_size),
+                        )),
+                    };
+                }
+                ui.with_layout(egui::Layout::top_down_justified(egui::Align::LEFT), |ui| {
+                    powered_by_egui_and_eframe(ui);
+                    egui::warn_if_debug_build(ui);
+                });
             });
-
         });
     }
 }
--- src/app/game_deck.rs
+++ src/app/game_deck.rs
@@ -5,10 +5,17 @@ pub mod game_deck {
 use rand::thread_rng;
     #[derive(Copy, Clone)]
     pub struct Card {
-        letter: char,
-        is_visible: bool,
+       pub letter: char,
+       pub is_visible: bool,
     }
     pub type Deck = [Card; 9];
+
+    pub fn get_scrambled_deck() -> Deck {
+        let mut deck = initial_deck();
+        deck.shuffle(&mut thread_rng());
+        deck
+    }
+
     pub fn initial_deck() -> Deck
     {
         return [
