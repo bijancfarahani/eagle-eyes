7967ec70f829294e9ac53273a21afdd8509b2395
Bijan Farahani <bijancfarahani@gmail.com>
2024-03-13 00:23:29 -0700

    change name in js files

--- assets/sw.js
+++ assets/sw.js
@@ -2,8 +2,8 @@ var cacheName = 'egui-template-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './eframe_template.js',
-  './eframe_template_bg.wasm',
+  './eagle_eyes_egui.js',
+  './eagle_eyes_egui.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
