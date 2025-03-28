b3059b10638ebffbeb74717065a34ba32330eed4
Bijan Farahani <bijancfarahani@gmail.com>
2025-03-27 19:59:56 -0700

    server URL

--- src/nakama.ts
+++ src/nakama.ts
@@ -9,7 +9,7 @@ class Nakama {
    isAuthenticated: boolean;
 
    async authenticate() {
-      this.client = new Client(process.env.NAKAMA_KEY, process.env.NAKAMA_URL, process.env.NAKAMA_PORT, false);
+      this.client = new Client(process.env.NAKAMA_KEY,  process.env.NAKAMA_URL,/*Cloudflare tunnel handles the port*/ "", false);
       this.deviceId = localStorage.getItem("deviceId");
       if (!this.deviceId) {
          this.deviceId = uuidv4();
